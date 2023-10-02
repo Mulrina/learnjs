29b5e93e0cd54b7b98d5cf4df8c8b56280c1f4a6
Marina <marinaterskova11@gmail.com>
2023-10-02 23:51:26 +0700

    Добавить задачу 2_11_4.js

---
+++ 2_11_4.js
@@ -0,0 +1,10 @@
+// Что выведет alert (И)? 
+
+alert( alert(1) && alert(2) ) // Вывод - 1, затем - undefined, так как alert не возвращет значение, то есть - undefined
+// поэтому до alert(2) дело просто не дойдет
+
+function main() {
+
+}
+
+main()

d2003ce69fb050af867cde2b27d67ec7fc6ec5d2
Marina <marinaterskova11@gmail.com>
2023-11-19 18:03:05 +0700

    Add ex.3_5_3

--- src/learnjsru/3_5_3.test.mjs
+++ src/learnjsru/3_5_3.test.mjs
@@ -1,18 +1,28 @@
 function filterRangeInPlace(arr, a, b) {
+  const deleteIndexes = []
   for (let i = 0; i < arr.length; i++) {
     let value = arr[i]
 
-    if (value < a && value > b) {
-      arr.slice(i, 1)
-      i--
+    if (value <= a || value >= b) {
+      deleteIndexes.push(i)
+      // arr.splice(i, 1)
+      // i--
     }
+  }
+  for (let i = 0; i < deleteIndexes.length; i++) {
+    const deleteIndex = deleteIndexes[i]
+    // 0, 1, 2, 3, 6
+    // [5, 4,6, 7, 3, 2, 1]
+    // 1
+    // [4, 6, 7, 3, 2, 1]
+    arr.splice(deleteIndex - i, 1)
   }
 }
 
 it('Фильтрация по диапазону "на месте"', () => {
   const arr = [5, 4, 6, 7, 3, 2, 1]
-  const result = filterRangeInPlace(arr, 1, 4)
+  filterRangeInPlace(arr, 1, 4)
 
-  console.log(result)
-  // expect(result).toBe([3, 2])
+  console.log(arr)
+  expect(arr).toStrictEqual([3, 2])
 })
